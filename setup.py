0b4853c05cf0665082a574b0d29842332633d243
David O'Connor <david.alan.oconnor@gmail.com>
2020-09-08 11:42:11 -0400

    Specify dependencies for Arduino

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Driver for the Anyleaf pH sensor",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/anyleaf/anyleaf_ph",
+    url="https://anyleaf.org",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
